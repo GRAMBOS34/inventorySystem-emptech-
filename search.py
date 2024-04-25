306f6ce079bdb1a4d7aada6fc7b49186b26b8411
GRAMBOS34 <vladimirivanjumawan@gmail.com>
2024-04-25 21:52:50 +0800

    put the search algo in a main function

--- search.py
+++ search.py
@@ -9,61 +9,62 @@ def remove_letters(text):
     return "".join(filter(str.isdigit, text))
 
 
-while True:
-  query = str(input("Search inventory: "))
-  with open('data.json', "r+") as file:
-    noBook = False
-    data = json.load(file)
+if __name__ == "__main__":
+    while True:
+        query = str(input("Search inventory: "))
+        with open('data.json', "r+") as file:
+            noBook = False
+            data = json.load(file)
 
-    #god's worst search algorithm
-    for i in data["Books"]:
-        if str(data['Books'][i]['Title']).lower() == query.lower():
+            #god's worst search algorithm
+            for i in data["Books"]:
+                if str(data['Books'][i]['Title']).lower() == query.lower():
 
-            #get the copies that are being borrowed
-            copyNums = []
-            for k in range(len(data['Books'][i]['copies'])):
-                if data['Books'][i]['copies'][k] == True:
-                    copyNums.append(k) 
-                else: continue
+                    #get the copies that are being borrowed
+                    copyNums = []
+                    for k in range(len(data['Books'][i]['copies'])):
+                        if data['Books'][i]['copies'][k] == True:
+                            copyNums.append(k) 
+                        else: continue
 
-            #if ever no one has borrowed a copy of the title
-            if len(copyNums) == 0:
-               print("No students have borrowed a copy of this title")
-               continue #because if i used break here, it would search for a borrower
+                    #if ever no one has borrowed a copy of the title
+                    if len(copyNums) == 0:
+                        print("No students have borrowed a copy of this title")
+                        continue #because if i used break here, it would search for a borrower
 
-            #make the full id
-            fullBookIds = []
-            for j in range(len(copyNums)):
-                fullBookIds.append(str(i) + copyNums[j])
-            
-            #find out who has the book
-            for user in data['Borrowers']:
-                for bookid in fullBookIds:
-                    if data['Borrowers'][user]['borrowedBook'] == bookid:
-                        copyNum = remove_letters(data['Borrowers'][user]['borrowedBook'])
-                        print(f"Student: {data['Borrowers'][user]['borrowerName']} has borrowed {data['Books'][i]['Title']} copy: {copyNum}")
+                    #make the full id
+                    fullBookIds = []
+                    for j in range(len(copyNums)):
+                        fullBookIds.append(str(i) + copyNums[j])
+                    
+                    #find out who has the book
+                    for user in data['Borrowers']:
+                        for bookid in fullBookIds:
+                            if data['Borrowers'][user]['borrowedBook'] == bookid:
+                                copyNum = remove_letters(data['Borrowers'][user]['borrowedBook'])
+                                print(f"Student: {data['Borrowers'][user]['borrowerName']} has borrowed {data['Books'][i]['Title']} copy: {copyNum}")
 
-        else: 
-            noBook = True
-            continue #because if i used break here, it would search for a borrower
+                else: 
+                    noBook = True
+                    continue #because if i used break here, it would search for a borrower
 
 
-    #this is just for the borrowers themselves
-    for j in data['Borrowers']:
-        if data['Borrowers'][j]['borrowerName'].lower() == query.lower():
-            if data['Borrowers'][j]['borrowedBook'] != None:
-                book = data['Borrowers'][j]['borrowedBook']
-                book = remove_numbers(book)
-                copyNum = remove_letters(data['Borrowers'][j]['borrowedBook'])
+            #this is just for the borrowers themselves
+            for j in data['Borrowers']:
+                if data['Borrowers'][j]['borrowerName'].lower() == query.lower():
+                    if data['Borrowers'][j]['borrowedBook'] != None:
+                        book = data['Borrowers'][j]['borrowedBook']
+                        book = remove_numbers(book)
+                        copyNum = remove_letters(data['Borrowers'][j]['borrowedBook'])
 
-                print(data['Books'][book]['Title'], f'Copy Number: {copyNum}')
+                        print(data['Books'][book]['Title'], f'Copy Number: {copyNum}')
 
-            else:
-               print("This student hasn't borrowed any books")
+                    else:
+                        print("This student hasn't borrowed any books")
 
-    #its like this because if there is no book, there is a guarantee that there is no borrower
-    #after all, how will you borrow something that doesnt exist?
-    if noBook == True:
-        #if it goes here then the search term doesn't exist
-        print("The item you are searching for does not exist.")
-        continue
+            #its like this because if there is no book, there is a guarantee that there is no borrower
+            #after all, how will you borrow something that doesnt exist?
+            if noBook == True:
+                #if it goes here then the search term doesn't exist
+                print("The item you are searching for does not exist.")
+                continue
