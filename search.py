219a4a70df29a851b798785900cd1b41f17fbcd6
GRAMBOS34 <vladimirivanjumawan@gmail.com>
2024-04-26 18:51:59 +0800

    fixed search algo

--- search.py
+++ search.py
@@ -11,21 +11,24 @@ def remove_letters(text):
 
 if __name__ == "__main__":
     while True:
+        noBook = False
         query = str(input("Search inventory: "))
         with open('data.json', "r+") as file:
-            noBook = False
             data = json.load(file)
+            numOfCopiesNotBorrowed = 0
 
             #god's worst search algorithm
             for i in data["Books"]:
                 if str(data['Books'][i]['Title']).lower() == query.lower():
-
+                
                     #get the copies that are being borrowed
                     copyNums = []
                     for k in range(len(data['Books'][i]['copies'])):
-                        if data['Books'][i]['copies'][k] == True:
+                        if data['Books'][i]['copies'][str(k)] == True:
                             copyNums.append(k) 
-                        else: continue
+                        else:
+                            numOfCopiesNotBorrowed = numOfCopiesNotBorrowed + 1 #adds 1 if a copy is not being borrowed
+                            continue
 
                     #if ever no one has borrowed a copy of the title
                     if len(copyNums) == 0:
@@ -35,7 +38,7 @@ if __name__ == "__main__":
                     #make the full id
                     fullBookIds = []
                     for j in range(len(copyNums)):
-                        fullBookIds.append(str(i) + copyNums[j])
+                        fullBookIds.append(str(i) + str(copyNums[j]))
                     
                     #find out who has the book
                     for user in data['Borrowers']:
@@ -43,14 +46,31 @@ if __name__ == "__main__":
                             if data['Borrowers'][user]['borrowedBook'] == bookid:
                                 copyNum = remove_letters(data['Borrowers'][user]['borrowedBook'])
                                 print(f"Student: {data['Borrowers'][user]['borrowerName']} has borrowed {data['Books'][i]['Title']} copy: {copyNum}")
+                                print(f"Number of copies of this book not borrowed: {numOfCopiesNotBorrowed}")
 
-                else: 
-                    noBook = True
-                    continue #because if i used break here, it would search for a borrower
+                    noBook = noBook #because for some reason python wont detect this as a call for the noBook variable declared at the top
+                    break
 
+                if query.lower() not in str(data['Books'][i]['Title']).lower():
+                    #done like this so the value won't alternate
+                    if noBook == True:
+                        continue
+
+                    else:
+                        noBook = not noBook
+                        continue
 
             #this is just for the borrowers themselves
             for j in data['Borrowers']:
+                if query.lower() not in data['Borrowers'][j]['borrowerName'].lower():
+                    #so the value won't alternate
+                    if noBook == True:
+                        continue
+
+                    else:
+                        noBook = not noBook
+                        continue
+
                 if data['Borrowers'][j]['borrowerName'].lower() == query.lower():
                     if data['Borrowers'][j]['borrowedBook'] != None:
                         book = data['Borrowers'][j]['borrowedBook']
@@ -58,10 +78,15 @@ if __name__ == "__main__":
                         copyNum = remove_letters(data['Borrowers'][j]['borrowedBook'])
 
                         print(data['Books'][book]['Title'], f'Copy Number: {copyNum}')
+                        noBook = not noBook #because if it gets out here this value will be true
+                        break
 
                     else:
                         print("This student hasn't borrowed any books")
-
+                        noBook = not noBook #because if it gets out here this value will be true
+                        break
+            
+            #placed here so that the next loop wont execute without passing through this check first
             #its like this because if there is no book, there is a guarantee that there is no borrower
             #after all, how will you borrow something that doesnt exist?
             if noBook == True:
